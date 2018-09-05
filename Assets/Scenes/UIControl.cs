3112be475779970c615702a00c47bc1f6ff7a390
Jiaju Ma <majiaju@hotmail.com>
2018-09-05 17:32:13 -0400

    Project uploaded to GitHub

---
+++ Assets/Scenes/UIControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+    private bool m_show = true;
+    private GameObject[] m_planes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void togglePlaneMesh () {
+
+        m_show = !m_show;
+
+        m_planes = GameObject.FindGameObjectsWithTag("PlaneGeneratedByARCore");
+
+        Debug.Log("toggled " + m_planes.Length);
+
+        foreach (GameObject plane in m_planes) {
+
+            Debug.Log("toggled plane");
+
+            plane.GetComponent<MeshRenderer>().enabled = m_show;
+ 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ARPoop
+
+* An Augmented Reality app that allows you to put 3D poop emoji on any surfaces.
+
+* Created by me using ARCore. In collaboration with Xiangyu Li.
